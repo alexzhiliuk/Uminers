f2b6ad58a2969af987cb47e8f1e345bd8b2821dc
ya_vyachko <konkursirus@gmail.com>
2024-06-06 19:46:00 +0300

    fixed progress block

--- src/js/modules/progress.js
+++ src/js/modules/progress.js
@@ -1,26 +1,7 @@
-const points = document.querySelectorAll('.point');
-
 const progressBar = document.querySelector('.progressBar__line_progress');
 
-const maxValue = 350;
-
 window.addEventListener('scroll', () => {
-<<<<<<< HEAD
-  // Вычисляем процент прокрутки страницы
-=======
->>>>>>> develop
   const scrollPercent = (window.pageYOffset / (document.documentElement.scrollHeight - document.documentElement.clientHeight)) * 70;
-
   progressBar.style.width = `${scrollPercent}%`;
-
-  points.forEach((point, index) => {
-    const pointValue = parseInt(point.querySelector('.point__heading').textContent);
-    const pointPercent = (pointValue / maxValue) * 100;
-
-    if (scrollPercent >= pointPercent) {
-      point.classList.add('active');
-    } else {
-      point.classList.remove('active');
-    }
-  });
 });
+
