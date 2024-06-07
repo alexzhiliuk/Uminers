c134b50ba2f32aa85f861176d212fd2415d28c70
alexandrzhyliuk <alexzhyliuk3@gmail.com>
2024-06-07 14:57:59 +0300

    articles slider

---
+++ src/js/modules/articlesSlider.js
@@ -0,0 +1,33 @@
+import Swiper, {Autoplay, Navigation, Pagination} from 'swiper';
+Swiper.use([Autoplay, Navigation, Pagination]);
+
+
+
+const swiperHero = new Swiper('#articlesSlider', {
+    slidesPerView: 1.1,
+    spaceBetween: 20,
+    speed: 1500,
+    freeMode: true,
+
+    navigation: {
+        nextEl: '#articlesSliderNext',
+        prevEl: '#articlesSliderPrev',
+    },
+
+    breakpoints: {
+        576: {
+            slidesPerView: 1.8,
+        },
+        768: {
+            slidesPerView: 2.5,
+        },
+        992: {
+            slidesPerView: 3,
+        }
+    }
+    
+});
+
+
+
+
