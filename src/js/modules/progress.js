02274e6a9ae0894b1895fb48203c217e0cd14d5b
ya_vyachko <konkursirus@gmail.com>
2024-06-08 14:23:28 +0300

    adaptive

--- src/js/modules/progress.js
+++ src/js/modules/progress.js
@@ -1,7 +1,9 @@
 const progressBar = document.querySelector('.progressBar__line_progress');
-
-window.addEventListener('scroll', () => {
-  const scrollPercent = (window.pageYOffset / (document.documentElement.scrollHeight - document.documentElement.clientHeight)) * 70;
-  progressBar.style.width = `${scrollPercent}%`;
-});
-
+try {
+  window.addEventListener('scroll', () => {
+    const scrollPercent = (window.pageYOffset / (document.documentElement.scrollHeight - document.documentElement.clientHeight)) * 70;
+    progressBar.style.width = `${scrollPercent}%`;
+  });
+} catch{
+  console.clear();
+}
