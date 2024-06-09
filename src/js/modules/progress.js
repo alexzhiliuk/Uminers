eca4edbcded47d7c190389fb1eafa3ffe61a7fae
ya_vyachko <konkursirus@gmail.com>
2024-06-09 12:47:05 +0300

    fix: progress-bar temp faster

--- src/js/modules/progress.js
+++ src/js/modules/progress.js
@@ -1,9 +1,9 @@
-const progressBar = document.querySelector('.progressBar__line_progress');
-try {
-  window.addEventListener('scroll', () => {
-    const scrollPercent = (window.pageYOffset / (document.documentElement.scrollHeight - document.documentElement.clientHeight)) * 70;
-    progressBar.style.width = `${scrollPercent}%`;
-  });
-} catch{
-  console.clear();
-}
+const graphicLine = document.querySelector('.progressBar__line_progress');
+const container = document.querySelector('.progressBar')
+window.addEventListener('scroll', () => {
+    const graphicPosition = container.getBoundingClientRect();
+    console.log(graphicPosition);
+    const scrollProgress = ((window.innerHeight - graphicPosition.top)-300) / container.offsetHeight;
+    graphicLine.style.width = `${50 * scrollProgress}%`;
+});
+
