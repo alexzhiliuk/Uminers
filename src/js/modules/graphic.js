6a48bde07abac54aae9774af2123c9f4f64876ef
alexandrzhyliuk <alexzhyliuk3@gmail.com>
2024-06-19 13:32:49 +0300

    fix

--- src/js/modules/graphic.js
+++ src/js/modules/graphic.js
@@ -1,8 +1,10 @@
 const graphicLine = document.querySelector('.graphic__lineWrapper');
 const container = document.querySelector('.graphic__body')
-window.addEventListener('scroll', () => {
-    const graphicPosition = container.getBoundingClientRect();
-    console.log(graphicPosition);
-    const scrollProgress = (window.innerHeight - graphicPosition.top) / container.offsetHeight;
-    graphicLine.style.width = `${50 * scrollProgress}%`;
-});
+if (graphicLine && container) {
+    window.addEventListener('scroll', () => {
+        const graphicPosition = container.getBoundingClientRect();
+        console.log(graphicPosition);
+        const scrollProgress = (window.innerHeight - graphicPosition.top) / container.offsetHeight;
+        graphicLine.style.width = `${50 * scrollProgress}%`;
+    });
+}
--- src/js/modules/progress.js
+++ src/js/modules/progress.js
@@ -1,9 +1,11 @@
 const graphicLine = document.querySelector('.progressBar__line_progress');
 const container = document.querySelector('.progressBar')
-window.addEventListener('scroll', () => {
-    const graphicPosition = container.getBoundingClientRect();
-    console.log(graphicPosition);
-    const scrollProgress = ((window.innerHeight - graphicPosition.top)-300) / container.offsetHeight;
-    graphicLine.style.width = `${50 * scrollProgress / 4}%`;
-});
+if (graphicLine && container) {
+    window.addEventListener('scroll', () => {
+        const graphicPosition = container.getBoundingClientRect();
+        console.log(graphicPosition);
+        const scrollProgress = ((window.innerHeight - graphicPosition.top)-150) / container.offsetHeight;
+        graphicLine.style.width = `${65 * scrollProgress / 4}%`;
+    });
+}
 
