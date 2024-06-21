afd85a1366cf6ff05ae8cb73b18c6f4e75bc8765
ya_vyachko <konkursirus@gmail.com>
2024-06-21 19:29:57 +0300

    hot-fix: fixed progress and map width

--- src/js/modules/progress.js
+++ src/js/modules/progress.js
@@ -1,11 +1,30 @@
 const graphicLine = document.querySelector('.progressBar__line_progress');
 const container = document.querySelector('.progressBar')
-if (graphicLine && container) {
-    window.addEventListener('scroll', () => {
-        const graphicPosition = container.getBoundingClientRect();
-        console.log(graphicPosition);
-        const scrollProgress = ((window.innerHeight - graphicPosition.top)-150) / container.offsetHeight;
-        graphicLine.style.width = `${65 * scrollProgress / 4}%`;
-    });
+
+const progress = () => {
+    if (document.body.offsetWidth < 1600) {
+        if (graphicLine && container) {
+            window.addEventListener('scroll', () => {
+                const graphicPosition = container.getBoundingClientRect();
+                const scrollProgress = ((window.innerHeight - graphicPosition.top) - 150) / container.offsetHeight;
+                graphicLine.style.width = `${65 * scrollProgress / 4}%`;
+            });
+        }
+    }
+    else {
+        graphicLine.style.maxWidth = 60 + 'vw';
+        if (graphicLine && container) {
+            window.addEventListener('scroll', () => {
+                const graphicPosition = container.getBoundingClientRect();
+                const scrollProgress = ((window.innerHeight - graphicPosition.top) - 150) / container.offsetHeight;
+                graphicLine.style.width = `${65 * scrollProgress / 4}%`;
+            });
+        }
+    }
 }
 
+progress();
+
+window.addEventListener('resize', () => {
+    progress();
+})
