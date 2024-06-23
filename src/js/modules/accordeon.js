aceb91ca549b1f856bd3c7020183492a5740615a
ya_vyachko <konkursirus@gmail.com>
2024-06-24 00:22:02 +0300

    added custom checkboxes and some fixes

--- src/js/modules/accordeon.js
+++ src/js/modules/accordeon.js
@@ -1,10 +1,14 @@
-let options = document.querySelectorAll('.options')
+let options = document.querySelectorAll('.options');
+
 options.forEach(elem => {
-    elem.addEventListener('click', () => {
-        if (!elem.classList.contains('active')) {
-            elem.classList.add('active')
-        }else{
-            elem.classList.remove('active')
-        }
-    })
-})
+  elem.addEventListener('click', () => {
+    if (!elem.classList.contains('active')) {
+      options.forEach(option => option.classList.remove('active'));
+      elem.classList.add('active');
+    } else {
+      if (event.target === elem.children[0]) {
+        elem.classList.remove('active');
+      }
+    }
+  });
+});
