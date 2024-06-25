764428bcce13dbb3bf1b668480ef8aef6c5872ec
alexandrzhyliuk <alexzhyliuk3@gmail.com>
2024-06-25 18:16:10 +0300

    fix img slider

--- src/js/modules/productSlider.js
+++ src/js/modules/productSlider.js
@@ -22,7 +22,7 @@ $(".product__gallery-img").click(function() {
     $(this).parents(".swiper-wrapper").find(".product__gallery-img").removeClass("product__gallery-img_active")
     $(this).addClass("product__gallery-img_active")
 
-    $(this).parents(".product__images").find(".card__image img").attr("src", $(this).find("img").attr("src"))
+    $(this).parents(".product__images").find(".card__image img").attr("src", $(this).find("img").attr("data-src"))
 })
 
 
