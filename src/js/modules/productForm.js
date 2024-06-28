7c0cd76e92db83f5c7942f4a98f50eefb3c30db9
ya_vyachko <konkursirus@gmail.com>
2024-06-28 03:44:40 +0300

    init + cart first page and some form

--- src/js/modules/productForm.js
+++ src/js/modules/productForm.js
@@ -1,41 +1,49 @@
 //input:text
 try {
     let form = document.querySelector('form.form')
-    let minus = document.querySelector('.form__symbol_left')
-    let plus = document.querySelector('.form__symbol_right')
-    let input = document.querySelector('input[name="ammount"]')
 
-    input.value = 1;
 
-    form.onsubmit = (e)=>{
-        if(!input.value){
-            input.value = 1;
+    let inputs = document.querySelectorAll('.form__ammount');
+    inputs.forEach(element=>{
+        let minus = element.querySelector('.form__symbol_left')
+        let plus = element.querySelector('.form__symbol_right')
+        let input = element.querySelector('input[name="ammount"]')
+        console.log(element, minus, plus, input);
+
+        input.value = 1;
+
+        form.onsubmit = (e)=>{
+            if(!input.value){
+                input.value = 1;
+            }
+            e.preventDefault();
         }
-        e.preventDefault();
-    }
-
-    input.addEventListener('keypress', (event) => {
-        if (!/\d/.test(event.key)) {
-        event.preventDefault();
-        }
-    });
     
-
-    minus.addEventListener('click', () => {
-        if (input.value>=2 && typeof +input.value === 'number') {
-            input.value = +input.value - 1;
-        }else{
-            input.value = 1
-        }
+        input.addEventListener('keypress', (event) => {
+            if (!/\d/.test(event.key)) {
+            event.preventDefault();
+            }
+        });
+        
+    
+        minus.addEventListener('click', () => {
+            if (input.value>=2 && typeof +input.value === 'number') {
+                input.value = +input.value - 1;
+            }else{
+                input.value = 1
+            }
+        })
+    
+        plus.addEventListener('click', () => {
+            if (input.value && typeof +input.value === 'number'){
+                input.value = +input.value + 1;
+            }else{
+                input.value = 1;
+            }
+        })
+    
     })
 
-    plus.addEventListener('click', () => {
-        if (input.value && typeof +input.value === 'number'){
-            input.value = +input.value + 1;
-        }else{
-            input.value = 1;
-        }
-    })
 
     //input:radio
     let tabs = document.querySelectorAll('.form__tab');
