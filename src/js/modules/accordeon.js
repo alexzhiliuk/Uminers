0938d43b03d335d737f36031ba01f21473f88714
alexandrzhyliuk <alexzhyliuk3@gmail.com>
2024-06-28 17:21:21 +0300

    sidebar

--- src/js/modules/accordeon.js
+++ src/js/modules/accordeon.js
@@ -3,7 +3,6 @@ let options = document.querySelectorAll('.options');
 options.forEach(elem => {
   elem.addEventListener('click', () => {
     if (!elem.classList.contains('active')) {
-      options.forEach(option => option.classList.remove('active'));
       elem.classList.add('active');
     } else {
       if (event.target === elem.children[0]) {
