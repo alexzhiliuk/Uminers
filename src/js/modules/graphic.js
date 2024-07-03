06b69d2101edcacf8802b29305200ead06efdaac
alexandrzhyliuk <alexzhyliuk3@gmail.com>
2024-07-03 19:00:20 +0300

    animation

--- src/js/modules/graphic.js
+++ src/js/modules/graphic.js
@@ -3,7 +3,7 @@ const container = document.querySelector('.graphic__body')
 if (graphicLine && container) {
     window.addEventListener('scroll', () => {
         const graphicPosition = container.getBoundingClientRect();
-        console.log(graphicPosition);
+        // console.log(graphicPosition);
         const scrollProgress = (window.innerHeight - graphicPosition.top) / container.offsetHeight;
         graphicLine.style.width = `${50 * scrollProgress}%`;
     });
