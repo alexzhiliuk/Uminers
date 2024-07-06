48e4fed3c0a9b4d67093ca68c1a2cbc79dc37ffd
ya_vyachko <konkursirus@gmail.com>
2024-07-06 19:53:04 +0300

    calculator page with adaptive

---
+++ src/js/modules/calculator-scrolls.js
@@ -0,0 +1,8 @@
+document.querySelectorAll('.scroll-link-calculator').forEach(item => {
+    item.addEventListener('click', (e) => {
+      e.preventDefault();
+      const targetId = item.getAttribute('href').substring(1);
+      const targetElement = document.getElementById(targetId);
+      targetElement.scrollIntoView({ behavior: 'smooth' });
+    });
+  });
