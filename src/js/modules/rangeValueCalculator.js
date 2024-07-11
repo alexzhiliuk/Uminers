85c4c4da74910743e600025acc6bafb34ea72109
ya_vyachko <konkursirus@gmail.com>
2024-07-11 13:59:51 +0300

    new calculator fixes

--- src/js/modules/rangeValueCalculator.js
+++ src/js/modules/rangeValueCalculator.js
@@ -1,10 +1,7 @@
 try{
 const quantityRangeInput = document.getElementById('quantity-range');
-const powerRangeInput = document.getElementById('power-range');
 const quantityRangeValue = document.getElementById('quantity-value');
-const powerRangeValue = document.getElementById('power-value');
 quantityRangeInput.parentElement.style.marginBottom = `${quantityRangeValue.offsetHeight+15}px`
-powerRangeInput.parentElement.style.marginBottom = `${powerRangeValue.offsetHeight+15}px`
 
 quantityRangeInput.addEventListener('input', function() {
   const value = this.value;
@@ -14,19 +11,6 @@ quantityRangeInput.addEventListener('input', function() {
   
   quantityRangeValue.innerHTML = value;
   quantityRangeValue.style.left = `calc(${percent}% - ${Math.ceil(quantityRangeValue.offsetWidth/2)}px)`;
-});
-
-powerRangeInput.addEventListener('input', function() {
-  const value = this.value;
-  const min = this.min || 0;
-  const max = this.max || 99;
-  const percent = ((value - min) / (max - min)) * 99;
-  
-  powerRangeValue.innerHTML = value;
-  powerRangeValue.style.left = `calc(${percent}% - ${powerRangeValue.offsetWidth/2}px)`;
-  if (window.innerWidth < 480) {
-    document.querySelector('button[type="submit"]').textContent = "Подробный расчет"
-}
 });
 
 
