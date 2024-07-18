9d33fce8d676a7ade1b543c70a05251581a06452
ya_vyachko <konkursirus@gmail.com>
2024-07-18 16:59:34 +0300

    hosting-test

--- src/js/modules/hosting-map.js
+++ src/js/modules/hosting-map.js
@@ -1,4 +1,4 @@
-
+try{
 document.addEventListener('DOMContentLoaded', function () {
     if (window.innerWidth < 768) {
         const overlayPoints = document.querySelectorAll('.overlay-point');
@@ -14,15 +14,60 @@ document.addEventListener('DOMContentLoaded', function () {
                         e.stopPropagation();
                         document.body.style.overflow = 'hidden';
                         document.querySelector('html').style.overflow = 'hidden';
+                        document.querySelector('.popup-shade').style.opacity ='1';
                         mobilePoint.classList.add('slide-in');
 
                         document.addEventListener('click', function (event) {
                             if (!mobilePoint.contains(event.target) && !overlayPoint.contains(event.target)) {
                                 mobilePoint.classList.remove('slide-in');
+                                document.querySelector('.popup-shade').style.opacity ='';
                                 document.body.style.overflow = '';
                                 document.querySelector('html').style.overflow = '';
                             }
                         });
+                    }
+                })
+
+            });
+        });
+    }
+});
+
+}catch(e){
+
+}
+document.addEventListener('DOMContentLoaded', function () {
+    if (window.innerWidth < 768) {
+        const overlayPoints = document.querySelectorAll('.overlay-point');
+        const overlayMobile = document.querySelectorAll('.overlay-point-popup');
+
+        overlayPoints.forEach((overlayPoint, index) => {
+            overlayPoint.addEventListener('click', function (e) {
+                overlayMobile.forEach(mobilePoint => {
+                    mobilePoint.classList.remove('slide-in')
+                    if (overlayPoint.dataset.country === mobilePoint.dataset.country) {
+                        console.log(overlayPoint.dataset.country)
+                        console.log(mobilePoint.dataset.country)
+                        e.stopPropagation();
+                        document.body.style.overflow = 'hidden';
+                        document.querySelector('html').style.overflow = 'hidden';
+                        document.querySelector('.popup-shade').style.opacity ='1';
+                        mobilePoint.classList.add('popup-show');
+
+                        document.addEventListener('click', function (event) {
+                            if (!mobilePoint.contains(event.target) && !overlayPoint.contains(event.target)) {
+                                mobilePoint.classList.remove('popup-show');
+                                document.querySelector('.popup-shade').style.opacity ='';
+                                document.body.style.overflow = '';
+                                document.querySelector('html').style.overflow = '';
+                            }
+                        });
+                        mobilePoint.querySelector('.cross').addEventListener('click',()=>{
+                                mobilePoint.classList.remove('popup-show');
+                                document.querySelector('.popup-shade').style.opacity ='';
+                                document.body.style.overflow = '';
+                                document.querySelector('html').style.overflow = '';
+                        })
                     }
                 })
 
